21059a54ec19ab074ae85fc35c8735938901fe1a
kmortyk <kmortyk@gmail.com>
2020-02-01 20:20:49 +0400

    inference2

---
+++ src/inference/src/load_graph.py
@@ -0,0 +1,64 @@
+FROZEN_GRAPH_PATH = './model/trt_graph.pb'
+IMAGE_PATH = './data/elephant.jpg'
+
+
+
+output_names = ['Logits/Softmax']
+input_names = ['input_1']
+
+import tensorflow as tf
+
+
+def get_frozen_graph(graph_file):
+    """Read Frozen Graph file from disk."""
+    with tf.gfile.FastGFile(graph_file, "rb") as f:
+        graph_def = tf.GraphDef()
+        graph_def.ParseFromString(f.read())
+    return graph_def
+
+
+trt_graph = get_frozen_graph(FROZEN_GRAPH_PATH)
+
+# Create session and load graph
+tf_config = tf.ConfigProto()
+tf_config.gpu_options.allow_growth = True
+tf_sess = tf.Session(config=tf_config)
+tf.import_graph_def(trt_graph, name='')
+
+
+# Get graph input size
+for node in trt_graph.node:
+    if 'input_' in node.name:
+        size = node.attr['shape'].shape
+        image_size = [size.dim[i].size for i in range(1, 4)]
+        break
+print("image_size: {}".format(image_size))
+
+# input and output tensor names.
+input_tensor_name = input_names[0] + ":0"
+output_tensor_name = output_names[0] + ":0"
+
+print("input_tensor_name: {}\noutput_tensor_name: {}".format(
+    input_tensor_name, output_tensor_name))
+
+output_tensor = tf_sess.graph.get_tensor_by_name(output_tensor_name)
+
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.applications.mobilenet_v2 import preprocess_input, decode_predictions
+
+# Optional image to test model prediction.
+img_path = IMAGE_PATH
+
+img = image.load_img(img_path, target_size=image_size[:2])
+x = image.img_to_array(img)
+x = np.expand_dims(x, axis=0)
+x = preprocess_input(x)
+
+feed_dict = {
+    input_tensor_name: x
+}
+preds = tf_sess.run(output_tensor, feed_dict)
+
+# decode the results into a list of tuples (class, description, probability)
+# (one such list for each sample in the batch)
+print('Predicted:', decode_predictions(preds, top=3)[0])
