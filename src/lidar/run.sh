b8549f4808e6210c31f2a9183d1a954ea3032b5d
kmortyk <casha.dryaba@mail.ru>
2020-02-01 17:00:32 +0400

    cleanup project

--- src/lidar/run.sh
+++
@@ -1,5 +0,0 @@
-source ~/ros_ws/vanilla.robotix.MikeCore/devel
-export ROS_PACKAGE_PATH=~/ros_ws:~/ros_ws/vanilla.robotix.MikeCore/lidar/lib:/opt/ros/melodic/share
-cd ../..
-catkin_make
-rosrun lidar
