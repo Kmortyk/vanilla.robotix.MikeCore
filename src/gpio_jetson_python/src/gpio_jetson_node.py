0ca7e0f80a21ad6152b2545b3533893613265ee3
Eldar <EldarK@outlook.com>
2020-02-09 17:54:47 +0400

    GPIO python node test

---
+++ src/gpio_jetson_python/src/gpio_jetson_node.py
@@ -0,0 +1,17 @@
+import rospy
+import Jetson.GPIO as GPIO
+from std_msgs.msg import String
+
+
+def callback(data):
+    rospy.loginfo(rospy.get_caller_id() + 'I heard %s', data.data)
+
+
+def listener():
+    rospy.init_node('gpio_jetson_node_python', anonymous=True)
+    rospy.Subscriber('gpio', String, callback)
+    rospy.spin()
+
+
+if __name__ == '__main__':
+    listener()
