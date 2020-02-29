65a8fdab36906073aabc95ae6d1771b3cdc53511
Eldar Kurbanov <EldarK@outlook.com>
2020-02-29 20:43:12 +0300

    Services: begin

---
+++ src/gpio_jetson_service/src/gpio_service.cpp
@@ -0,0 +1,16 @@
+//
+// Created by laborant on 29.02.2020.
+//
+
+#include "ros/ros.h"
+#include ""
+
+bool callback()
+
+int main(int argc, char **argv)
+{
+    ros::init(argc, argv, "gpio_service");
+    ros::NodeHandle nodeHandle;
+
+    ros::ServiceServer service = nodeHandle.advertiseService("gpio_service", )
+}
--- src/movement/src/movement_node.cpp
+++ src/movement/src/movement_node.cpp
@@ -6,19 +6,6 @@
 #include "pcl_ros/point_cloud.h"
 
 void ydLidarPointsCallback(const sensor_msgs::PointCloud2ConstPtr& message) {
-    /*pcl::PointCloud<pcl::PointXYZ> depth;
-    pcl::fromROSMsg(*message, depth);
-    int x = 0, y = 0; // set x and y
-    pcl::PointXYZ p1 = depth.at(x, y);
-    ROS_INFO("x = %f y = %f", p1.x, p1.y);*/
-    //ROS_INFO("size: %lu", message->width);
-    /*for (int j = 1; j < message->height; ++j) {
-        for (int k = 0; k < message->row_step; ++k) {
-            printf("%i ", message->data[j + k]);
-        }
-        //ROS_INFO("");
-        printf("\n");
-    }*/
     for (int j = 0; j < message->row_step * message->height; ++j) {
         printf("%i ", message->data[j]);
     }
