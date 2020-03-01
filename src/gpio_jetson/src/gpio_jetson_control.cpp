2381af98de1848f0aadc7c758d87ba4c612bbf6c
Eldar <EldarK@outlook.com>
2020-03-01 19:12:46 +0400

    try3

--- src/gpio_jetson/src/gpio_jetson_control.cpp
+++ src/gpio_jetson/src/gpio_jetson_control.cpp
@@ -10,37 +10,36 @@ int main(int argc, char **argv)
     ros::init(argc, argv, "gpio_control");
     ros::NodeHandle nodeHandle;
     ros::Publisher publisher = nodeHandle.advertise<std_msgs::String>("gpio", 1000);
-    std::stringstream ss;
     while (ros::ok())
     {
+        std::string ss;
         std_msgs::String msg;
         char c;
         std::cin >> c;
         switch (c) {
             case 'w':
                 ROS_INFO("forward");
-                ss << "forward";
+                ss = "forward";
                 break;
             case 'a':
                 ROS_INFO("left");
-                ss << "left";
+                ss = "left";
                 break;
             case 's':
                 ROS_INFO("backward");
-                ss << "backward";
+                ss = "backward";
                 break;
             case 'd':
                 ROS_INFO("right");
-                ss << "right";
+                ss = "right";
                 break;
             default:
                 ROS_INFO("stop");
-                ss << "stop";
+                ss = "stop";
                 break;
         }
-        msg.data = ss.str();
+        msg.data = ss;
         publisher.publish(msg);
-        ss.clear();
         ros::spinOnce();
     }
     return 0;
