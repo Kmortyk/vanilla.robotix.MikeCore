10dff3ae1d091094ef229191460050cac6a4dc1c
Eldar <EldarK@outlook.com>
2020-03-01 18:59:06 +0400

    gpio control from keyboard

--- src/gpio_jetson/src/gpio_jetson_control.cpp
+++ src/gpio_jetson/src/gpio_jetson_control.cpp
@@ -2,9 +2,40 @@
 // Created by eldar on 01.03.2020.
 //
 
+#include <std_msgs/String.h>
 #include "ros/ros.h"
 
 int main(int argc, char **argv)
 {
+    ros::init(argc, argv, "gpio_control");
+    ros::NodeHandle nodeHandle;
+    ros::Publisher publisher = nodeHandle.advertise<std_msgs::String>("gpio_jetson_node", 1000);
+    std::stringstream ss;
+    while (ros::ok())
+    {
+        std_msgs::String msg;
+        char c;
+        std::cin >> c;
+        switch (c) {
+            case 'w':
+                ss << "forward";
+                break;
+            case 'a':
+                ss << "left";
+                break;
+            case 's':
+                ss << "backward";
+                break;
+            case 'd':
+                ss << "right";
+                break;
+            default:
+                ss << "stop";
+                break;
+        }
+        msg.data = ss.str();
+        publisher.publish(msg);
+        ss.clear();
+    }
     return 0;
 }
