07ac3e342ea08109f823dc9ba500941395e92950
Eldar <EldarK@outlook.com>
2020-03-21 18:25:44 +0400

    Robot moving try 1

--- src/movement/src/movement_node.cpp
+++ src/movement/src/movement_node.cpp
@@ -2,12 +2,19 @@
 // Created by eldar on 23.02.2020.
 //
 
+#include <sensor_msgs/LaserScan.h>
 #include "ros/ros.h"
 #include "pcl_ros/point_cloud.h"
+#include "gpio_jetson_service/gpio_srv.h"
+#include "../../gpio_jetson_service/include/gpio_jetson_service/commands.hpp"
+#include <vector>
 
-void ydLidarPointsCallback(const sensor_msgs::PointCloud2ConstPtr& message) {
-    for (int j = 0; j < message->row_step * message->height; ++j) {
-        printf("%i ", message->data[j]);
+bool crashing = false;
+
+void ydLidarPointsCallback(const sensor_msgs::LaserScanConstPtr& message) {
+    if (message->ranges[360] > 0 && message->ranges[360] < 0.2f) {
+        ROS_WARN("DANGER!!");
+        crashing = true;
     }
 }
 
@@ -15,6 +22,20 @@ int main(int argc, char **argv) {
     ros::init(argc, argv, "movement");
     ros::NodeHandle nodeHandle;
     ros::Subscriber ydlidarPointsSub =
-            nodeHandle.subscribe<sensor_msgs::PointCloud2>("/scan_matched_points2", 1000, ydLidarPointsCallback);
-    ros::spin();
+            nodeHandle.subscribe<sensor_msgs::LaserScan>("/scan", 1000, ydLidarPointsCallback);
+    ros::ServiceClient client = nodeHandle.serviceClient<gpio_jetson_service::gpio_srv>("gpio_jetson_service");
+    while (ros::ok()) {
+        gpio_jetson_service::gpio_srv service;
+        if (crashing) {
+            service.request.command = MoveCommands::FULL_STOP;
+            sleep(1);
+            service.request.command = MoveCommands::RIGHT_FORWARD_FAST;
+            usleep(100);
+            crashing = false;
+        } else {
+            service.request.command = MoveCommands::FORWARD_FAST;
+        }
+        ros::spinOnce();
+    }
+    return EXIT_SUCCESS;
 }
