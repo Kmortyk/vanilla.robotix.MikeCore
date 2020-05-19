5af210add702281e87aa011529807ff1586acd80
Eldar Kurbanov <EldarK@outlook.com>
2020-05-19 21:13:58 +0400

    Unity package for two lidars ~

---
+++ src/ydlidar_unity/src/ydlidar_unity_node.cpp
@@ -0,0 +1,57 @@
+//
+// Created by eldar on 19.05.2020.
+//
+
+#include "ros/ros.h"
+#include "sensor_msgs/LaserScan.h"
+
+sensor_msgs::LaserScan::_ranges_type unityPointCloud;
+sensor_msgs::LaserScan::Ptr lastX4Message;
+
+void x4Callback(const sensor_msgs::LaserScan::Ptr x4Cloud)
+{
+    for (int i = 540; i < 720; i++)
+    {
+        unityPointCloud[i] = x4Cloud->ranges[i];
+    }
+    for (int i = 0; i < 180; i++)
+    {
+        unityPointCloud[i] = x4Cloud->ranges[i];
+    }
+    lastX4Message = x4Cloud;
+}
+
+void f4Callback(const sensor_msgs::LaserScan::ConstPtr f4Cloud)
+{
+    int j = 180;
+    for (int i = 540; i < 720; i++)
+    {
+        unityPointCloud[j] = f4Cloud->ranges[i];
+        j++;
+    }
+    for (int i = 0; i < 180; i++)
+    {
+        unityPointCloud[j] = f4Cloud->ranges[i];
+        j++;
+    }
+}
+
+int main(int argc, char **argv)
+{
+    ros::init(argc, argv, "ydlidar_unity_node");
+    ros::NodeHandle nodeHandle;
+    const ros::Subscriber f4Subscriber = nodeHandle.subscribe("scanF4", 1000, f4Callback);
+    const ros::Subscriber x4Subscriber = nodeHandle.subscribe("scanX4", 1000, x4Callback);
+    const ros::Publisher unityPublisher = nodeHandle.advertise<sensor_msgs::LaserScan>("scan", 1000);
+    while (ros::ok())
+    {
+        lastX4Message->ranges = unityPointCloud;
+        unityPublisher.publish(lastX4Message);
+        for (int i = 0; i < 720; ++i) {
+            std::cout << i << " ";
+        }
+        std::cout << std::endl;
+        ros::spinOnce();
+    }
+    return EXIT_SUCCESS;
+}
