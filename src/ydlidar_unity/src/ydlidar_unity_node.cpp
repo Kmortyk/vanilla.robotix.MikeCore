fb9ff8bd1505f6f70c9504154873b0bf212580fe
Eldar Kurbanov <EldarK@outlook.com>
2020-06-07 18:51:28 +0400

    X4 debug log

--- src/ydlidar_unity/src/ydlidar_unity_node.cpp
+++ src/ydlidar_unity/src/ydlidar_unity_node.cpp
@@ -11,6 +11,14 @@ sensor_msgs::LaserScan message;
 
 void x4Callback(const sensor_msgs::LaserScan x4Cloud)
 {
+
+    //POINT CLOUD VIEWER
+    for (int i = 0; i < 720; i++) {
+        if (!(i % 8))
+            std::cout << std::setfill('0') << std::setw(3) << std::floor(x4Cloud.ranges[i] * 10)/10 << " ";
+    }
+    std::cout << std::endl;
+
     /*
      * на этом лидаре всего 720 точек.
      * c 270 по 449 это слепая зона лидара
@@ -70,11 +78,11 @@ void f4Callback(const sensor_msgs::LaserScan::ConstPtr f4Cloud)
     ROS_INFO("A= %f; B= %f; C= %f; D= %f.", a, b, c, d);*/
 
     //POINT CLOUD VIEWER
-    for (int i = 0; i < 789; i++) {
+    /*for (int i = 0; i < 789; i++) {
         if (!(i % 8))
             std::cout << std::setfill('0') << std::setw(3) << std::floor(f4Cloud->ranges[i] * 10)/10 << " ";
     }
-    std::cout << std::endl;
+    std::cout << std::endl;*/
     //PUT IT TO TEXT EDITOR
     // 001 002 003 004 005 006 007 008 009 010 011 012 013 014 015 016 017 018 019 020 021 022 023 024 025 026 027 028 029 030 031 032 033 034 035 036 037 038 039 040 041 042 043 044 045 046 047 048 049 050 051 052 053 054 055 056 057 058 059 060 061 062 063 064 065 066 067 068 069 070 071 072 073 074 075 076 077 078 079 080 081 082 083 084 085 086 087 088 089 090 091 092 093 094 095 096 097 098 099
     //END POINT CLOUD VIEWER
