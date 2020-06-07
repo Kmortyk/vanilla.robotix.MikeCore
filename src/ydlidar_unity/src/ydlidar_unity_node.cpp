76df5848bf22ce0170a507a084c2813b30532050
Eldar Kurbanov <EldarK@outlook.com>
2020-06-07 17:55:22 +0400

    Unity lidar big fix for F4

--- src/ydlidar_unity/src/ydlidar_unity_node.cpp
+++ src/ydlidar_unity/src/ydlidar_unity_node.cpp
@@ -11,6 +11,15 @@ sensor_msgs::LaserScan message;
 
 void x4Callback(const sensor_msgs::LaserScan x4Cloud)
 {
+    /*
+     * на этом лидаре всего 720 точек.
+     * c 270 по 449 это слепая зона лидара
+     * с 90 по 269 это левая часть робота
+     * с 450 по 629 это правая часть робота
+     * с 630 по 718 и с 0 по 89 это передняя часть робота
+     * это базовый лидар поэтому массив из F4 надо подгонять под него
+     */
+
     for (int i = 540; i < 720; i++)
     {
         unityPointCloud[i] = x4Cloud.ranges[i];
@@ -70,15 +79,35 @@ void f4Callback(const sensor_msgs::LaserScan::ConstPtr f4Cloud)
     // 001 002 003 004 005 006 007 008 009 010 011 012 013 014 015 016 017 018 019 020 021 022 023 024 025 026 027 028 029 030 031 032 033 034 035 036 037 038 039 040 041 042 043 044 045 046 047 048 049 050 051 052 053 054 055 056 057 058 059 060 061 062 063 064 065 066 067 068 069 070 071 072 073 074 075 076 077 078 079 080 081 082 083 084 085 086 087 088 089 090 091 092 093 094 095 096 097 098 099
     //END POINT CLOUD VIEWER
 
+    /*
+     * результаты эксперимента:
+     * всего лидар выдаёт 789 точек
+     * c 0 по 174 и с 650 по 789 это слепая зона лидара
+     * с 175 по 299 это левая часть робота
+     * с 300 по 519 это задняя часть робота
+     * с 520 по 649 это правая часть робота
+     * решено взять следующие диапазоны
+     * лево 192 по 282 (90 элементов)
+     * право (520+649) / 2 = 584. 584-45=539 по 539+90 = 629 (90 элементов)
+     * зад с 319 по 409 (180 элементов)
+     */
+
     int j = 180;
-    for (int i = 540; i < 720; i++)
+    for (int i = 192; i < 282; i++)
     {
         unityPointCloud[j] = f4Cloud->ranges[i];
         unityIntensities[j] = f4Cloud->intensities[i];
         j++;
     }
-    for (int i = 0; i < 180; i++)
+    for (int i = 319; i < 499; i++)
     {
+        unityPointCloud[j] = f4Cloud->ranges[i];
+        unityIntensities[j] = f4Cloud->intensities[i];
+        j++;
+    }
+    for (int i = 539; i < 629; i++)
+    {
+        unityPointCloud[j] = f4Cloud->ranges[i];
         unityIntensities[j] = f4Cloud->intensities[i];
         j++;
     }
@@ -97,9 +126,6 @@ int main(int argc, char **argv)
         message.intensities.clear();
         for (int i = 0; i < 719; i++)
         {
-            if (i < 0.1 && i > 0) {
-                ROS_WARN("Fault detected at %d", i);
-            }
             message.ranges.push_back(unityPointCloud[i]);
             message.intensities.push_back(unityIntensities[i]);
         }
