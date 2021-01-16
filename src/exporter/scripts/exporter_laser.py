2eeb58bf06550deb6e06276625f26c7f822a7568
Eldar Kurbanov <EldarK@outlook.com>
2021-01-16 17:46:41 +0300

    Complete programm to collect data

--- src/exporter/scripts/exporter_laser.py
+++ src/exporter/scripts/exporter_laser.py
@@ -2,28 +2,74 @@
 import rospy
 import tf
 import pyautogui
-
+import numpy
+import os
 from sensor_msgs.msg import LaserScan
+from pathlib import Path
+
+screenshot_region = (0, 0, 300, 300)
+transforms = None
+rotations = None
+times = None
+lasers = None
+time_now = 0
+
+screenshots_folder = Path(os.path.realpath(__file__))\
+                         .parent.parent.as_posix() + os.sep + 'data' + os.sep + 'temp_screenshots' + os.sep
 
 
 def take_rviz_screenshot():
-    pyautogui.screenshot('screenshots/screen.jpg', region=(0, 0, 300, 300))
+    pyautogui.screenshot(screenshots_folder + str(time_now) + '.jpg', screenshot_region)
 
 
 def transform_listen():
+    global transform, rotation
     try:
-        (tranform, rotation) = transform_listener.lookupTransform('base_link',
-                                                                  'map',
-                                                                  rospy.Time(0))
-        rospy.loginfo("%f %f", tranform, rotation)
+        (transform, rotation) = transform_listener.lookupTransform('base_link',
+                                                                   'map',
+                                                                   rospy.Time(0))
+        print(transform, rotation)
     except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
         rospy.logerr("Exception when transform listen")
 
 
+def save_data(laser_data):
+    global transforms, rotations, times, lasers
+    transform_data = numpy.array([transform[0], transform[1]])
+    rotation_data = numpy.array(rotations)
+    time_data = [time_now]
+    if transforms is None:
+        transforms = numpy.array(transform_data)
+        rotations = numpy.array(rotation_data)
+        times = numpy.array(time_data)
+        lasers = numpy.array(laser_data)
+    else:
+        numpy.append(transforms, transform_data)
+        numpy.append(rotations, rotation_data)
+        numpy.append(times, time_data)
+        numpy.append(lasers, laser_data)
+
+
 def laser_callback(data):
+    global time_now
+    time_now = rospy.Time.now().to_nsec()
     transform_listen()
     take_rviz_screenshot()
     rospy.loginfo(data)
+    if transform is not None:
+        save_data(data)
+
+
+def finalize_data():
+    name_situation = raw_input("Enter name of this situation: ")
+    situation_folder = Path(os.path.realpath(__file__)) \
+        .parent.parent.as_posix() + os.sep + 'data' + os.sep + 'situations' + os.sep + name_situation + os.sep
+    os.mkdir(situation_folder)
+    os.mkdir(situation_folder + os.sep + 'screenshots' + os.sep)
+    numpy.savez(situation_folder + 'data.npz', transforms, rotations, times, lasers)
+    (_, _, filenames) = next(os.walk(screenshots_folder))
+    for filename in filenames:
+        os.rename(screenshots_folder + filename, situation_folder + 'screenshots' + os.sep + filename)
 
 
 if __name__ == '__main__':
@@ -31,3 +77,4 @@ if __name__ == '__main__':
     transform_listener = tf.TransformListener()
     rospy.Subscriber('scan', LaserScan, laser_callback)
     rospy.spin()
+    finalize_data()
