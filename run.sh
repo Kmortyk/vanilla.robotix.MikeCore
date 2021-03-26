a7039746292e72cc05ca6622bc83b9b063c530e6
Eldar <EldarK@outlook.com>
2021-03-26 21:01:12 +0300

    Remove useless packages

--- run.sh
+++
@@ -1,9 +0,0 @@
-#
-# run like:
-# $ . ./run.sh
-
-source devel/setup.bash
-export ROS_PACKAGE_PATH=~/vanilla.robotix.MikeCore:/opt/ros/melodic/share
-export ROS_MASTER_URI=http://192.168.100.3:11311
-catkin_make
-rosrun camera camera
