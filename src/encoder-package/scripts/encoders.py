50a1e3143b85a1a5c47f5594be149b99eab1a17b
Eldar <EldarK@outlook.com>
2021-05-04 18:37:19 +0300

    Direction detect

--- src/encoder-package/scripts/encoders.py
+++ src/encoder-package/scripts/encoders.py
@@ -16,6 +16,12 @@ value_l_b = 0
 value_r_f = 0
 value_r_b = 0
 
+ticker_l = 0
+ticker_r = 0
+
+direction_l = 0
+direction_r = 0
+
 tick21 = 0
 tick22 = 0
 tick23 = 0
@@ -23,27 +29,39 @@ tick24 = 0
 
 
 def gpio_callback21(channel):
-    global tick21, value_r_f
+    global tick21, value_r_f, ticker_r, direction_r
     tick21 += 1
     value_r_f = GPIO.input(21)
+    if ticker_r is 0:
+        direction_r = 1
+    ticker_r = 0
 
 
 def gpio_callback22(channel):
-    global tick22, value_l_f
+    global tick22, value_l_f, ticker_l, direction_l
     tick22 += 1
     value_l_f = GPIO.input(22)
+    if ticker_l is 0:
+        direction_l = 1
+    ticker_l = 0
 
 
 def gpio_callback23(channel):
-    global tick23, value_r_b
+    global tick23, value_r_b, ticker_r, direction_r
     tick23 += 1
     value_r_b = GPIO.input(23)
+    if ticker_r is 1:
+        direction_r = 0
+    ticker_r = 1
 
 
 def gpio_callback24(channel):
-    global tick24, value_l_b
+    global tick24, value_l_b, ticker_l, direction_l
     tick24 += 1
     value_l_b = GPIO.input(24)
+    if ticker_l is 1:
+        direction_l = 0
+    ticker_l = 1
 
 
 def main():
@@ -67,6 +85,7 @@ def main():
         print("23 ", tick23)
         print("24 ", tick24)
         print(value_l_f, " ", value_l_b, " ", value_r_f, " ", value_r_b)
+        print(direction_l, direction_r)
     GPIO.cleanup({21, 22, 23, 24})
 
 
